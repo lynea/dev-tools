a6113470f862439979f1af32f61cb3d074717c7a
Rene <rene@Renes-MacBook-Pro.local>
2024-03-12 21:47:36 +0100

    fix: revert changes to auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,6 @@
-import { authMiddleware, redirectToSignIn } from '@clerk/nextjs'
-import { NextResponse } from 'next/server'
+import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-    afterAuth(auth, req, evt) {
-        // Handle users who aren't authenticated
-        if (!auth.userId && !auth.isPublicRoute) {
-            return redirectToSignIn({ returnBackUrl: req.url })
-        }
-        // Redirect signed in users to organization selection page if they are not active in an organization
-        if (
-            auth.userId &&
-            !auth.orgId &&
-            req.nextUrl.pathname !== '/account/organization/create'
-        ) {
-            const orgSelection = new URL(
-                '/account/organization/create',
-                req.url
-            )
-            return NextResponse.redirect(orgSelection)
-        }
-        // If the user is signed in and trying to access a protected route, allow them to access route
-        if (auth.userId && !auth.isPublicRoute) {
-            return NextResponse.next()
-        }
-        // Allow users visiting public routes to access them
-        return NextResponse.next()
-    },
-
     publicRoutes: [
         '/api/todos/(.*)',
         '/api/webhooks(.*)',
